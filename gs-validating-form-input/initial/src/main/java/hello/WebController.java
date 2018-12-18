b36706e12dcf11c05cc892cd8b43ad3f05b89c4a
Ben Lin <BenL@shelter.com>
2018-12-18 15:12:02 -0600

    added executable, finished with validation form.

--- gs-validating-form-input/initial/src/main/java/hello/WebController.java
+++ gs-validating-form-input/initial/src/main/java/hello/WebController.java
@@ -13,7 +13,7 @@ import javax.validation.Valid;
 public class WebController implements WebMvcConfigurer {
 
     @Override
-    public void addViewControllers(ViewControllerRegistry registry){
+    public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/results").setViewName("results");
     }
 
